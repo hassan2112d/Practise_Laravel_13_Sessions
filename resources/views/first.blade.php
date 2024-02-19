9aa9f1e80fe9c29905f838b6cb4252fcbdb50dc0
hassan2112d <hassan2112d@aptechgdn.net>
2024-02-19 17:53:39 +0500

    hello

--- resources/views/first.blade.php
+++ resources/views/first.blade.php
@@ -1,1 +1,24 @@
-<h2> Hello_World </h2>
+<h2>Hello_World</h2>
+
+{{"hello"}}
+
+{!! "<script>alert('hiii');</script>"!!}
+
+<br>
+{{5+9}}
+
+@php
+    $user = ["Hassan","Ali"];
+@endphp
+
+<br>
+
+<ul>
+    @if(count($user) > 0)
+        @foreach ($user as $users)
+            <li>{{$loop->iteration}}: {{$users}}</li>
+        @endforeach
+    @else
+        <li><h1>No data</h1></li>
+    @endif
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -28,10 +28,11 @@ Route::get('/demo', function(){
 
 // if id is necessary
 //Defing the type of parameter of url
-Route::get('/post/{id}', function(string $value ){
+// Route::get('/post/{id}', function(string $value ){
 
-    return '<h1>Post Id : '.$value.'';
-})->whereIn('id',[7,9,8,'movie']);
+//     return '<h1>Post Id : '.$value.'';
+// })->whereIn('id',[7,9,8,'movie']);
+
 
 
 //If id is not necessary
@@ -48,36 +49,37 @@ Route::get('/post/{id}', function(string $value ){
 
 //Giving the particular name to the url of the route
 
-Route::get('/about-us', function(){
+// Route::get('/about-us', function(){
 
-    return view('about');
-})->name('aboutpage');
+//     return view('about');
+// })->name('aboutpage');
+
 
 //Using Prefexix to not to give again the again name .of xyz page caategories
 
-Route::prefix('/page')->group(function(){
+// Route::prefix('/page')->group(function(){
 
-    Route::get('/shoes', function(){
+//     Route::get('/shoes', function(){
 
-        return view('shoes');
-    });
-    Route::get('/dress', function(){
+//         return view('shoes');
+//     });
+//     Route::get('/dress', function(){
 
-        return view('dress');
-    })->name('dress');
+//         return view('dress');
+//     })->name('dress');
 
 
-});
+// });
 
 //It will redirect it to the updated page
 
-Route::redirect('/hello','/about-us',301);
+// Route::redirect('/hello','/about-us',301);
 
 // last number tells that perment or temporaily
 
 
 //It will redirect to default 
-Route::fallback(function(){
+// Route::fallback(function(){
 
-    return "<h2>Page Not found";
-});
+//     return "<h2>Page Not found";
+// });
